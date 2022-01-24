c42a58f57d784bbbc43b9f80d6b29305d3ca404a
michelleroos <hello.michelleroos@gmail.com>
2022-01-24 09:27:41 -0500

    tree levels

---
+++ _tree-lvls.js
@@ -0,0 +1,125 @@
+/*
+tree levels
+Write a function, treeLevels, that takes in the root of a binary tree. The function should return a 2-Dimensional array where each subarray represents a level of the tree.
+*/
+
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+const treeLevels = (root) => {
+  const levels = [];
+  if (!root) return levels;
+  const q = [[root, 0]];
+  while (q.length) {
+    const [node, level] = q.shift();
+    if (level === levels.length) {
+      levels.push([node.val]);
+    } else {
+      levels[level].push(node.val);
+    };
+    if (node.left) q.push([node.left, level + 1]);
+    if (node.right) q.push([node.right, level + 1]);
+  };
+  return levels;
+};
+
+test_00:
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+
+treeLevels(a); // ->
+// [
+//   ['a'],
+//   ['b', 'c'],
+//   ['d', 'e', 'f']
+// ]
+test_01:
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+const g = new Node("g");
+const h = new Node("h");
+const i = new Node("i");
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+e.left = g;
+e.right = h;
+f.left = i;
+
+//         a
+//      /    \
+//     b      c
+//   /  \      \
+//  d    e      f
+//      / \    /
+//     g  h   i
+
+treeLevels(a); // ->
+// [
+//   ['a'],
+//   ['b', 'c'],
+//   ['d', 'e', 'f'],
+//   ['g', 'h', 'i']
+// ]
+test_02:
+const q = new Node("q");
+const r = new Node("r");
+const s = new Node("s");
+const t = new Node("t");
+const u = new Node("u");
+const v = new Node("v");
+
+q.left = r;
+q.right = s;
+r.right = t;
+t.left = u;
+u.right = v;
+
+//      q
+//    /   \
+//   r     s
+//    \
+//     t
+//    /
+//   u
+//  /
+// v
+
+treeLevels(q); //->
+// [
+//   ['q'],
+//   ['r', 's'],
+//   ['t'],
+//   ['u'],
+//   ['v']
+// ]
+test_03:
+treeLevels(null); // -> []

dab92a8732ab703a38753114be494e5afa27dbec
michelleroos <hello.michelleroos@gmail.com>
2022-02-16 16:21:00 -0500

    valid pali II

--- max-depth.js
+++ max-depth.js
@@ -12,6 +12,11 @@ var maxDepth = function (root) {
   return 1 + Math.max(left, right)
 };
 
+/*
+Runtime: 60 ms, faster than 99.56% of JavaScript online submissions for Maximum Depth of Binary Tree.
+Memory Usage: 44.8 MB, less than 27.79% of JavaScript online submissions for Maximum Depth of Binary Tree.
+*/
+
 const root1 = [3,9,20,null,null,15,7];
 console.log(maxDepth(root1));
 // Output: 3
